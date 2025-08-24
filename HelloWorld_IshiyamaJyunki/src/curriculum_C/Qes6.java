a0cb088dd721840ccf6ff23766112a2c50f2dd11
l218008e <l218008e@l218008e.kankyo.local>
2025-08-25 00:56:31 +0900

    Qes6の課題が完了しました。確認お願い致します

---
+++ HelloWorld_IshiyamaJyunki/src/curriculum_C/Qes6.java
@@ -0,0 +1,66 @@
+package curriculum_C;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Qes6 {
+
+	public static void main(String[] args) {
+		Player player = new Player();
+		CPU cpu = new CPU();
+		
+		boolean win = false;
+		
+		while (!win) {
+			int pHand = player.chooseHand();
+			int cHand = cpu.chooseHand();
+			
+			if (pHand == cHand) {
+				System.out.println("あいこです。");
+			} else if ((pHand == 0 && cHand == 1) ||
+					   (pHand == 1 && cHand == 2) ||
+					   (pHand == 2 && cHand == 0)) {
+				System.out.println("あなたの勝ち。");
+				win = true;
+			}else {
+				System.out.println("あなたの負け。");
+			}
+		}
+		System.out.println("ゲームは終了しました。");
+	}
+}
+
+class Player {
+	private Scanner scanner = new Scanner(System.in);
+	private String[] hands = { "グー", "チョキ", "パー" };
+
+	public int chooseHand() {
+		System.out.print("グー(0),チョキ(1),パー(2) 選んで入力:");
+		int choice = scanner.nextInt();
+		if (choice < 0 || choice > 2) {
+			System.out.println("0～2を入力してください");
+			return chooseHand();
+		}
+		System.out.println("あなたの手:" + hands[choice]);
+        return choice;
+	}
+
+	public String getHandName(int hand) {
+		return hands[hand];
+	}
+}
+
+class CPU {
+	private Random random = new Random();
+	private String[] hands = {"グー","チョキ","パー"};
+	
+	public int chooseHand() {
+		int choice = random.nextInt(3);
+		System.out.println("CPUの手:" + hands[choice]);
+		return choice;
+	}
+	
+	public String getHandName(int hand) {
+		return hands[hand];
+	}
+}
