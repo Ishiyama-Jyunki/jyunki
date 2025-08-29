385befc789277ae63203132ba2f0237061b48ddb
l218008e <l218008e@l218008e.kankyo.local>
2025-08-29 09:21:37 +0900

    Animalクラスを作成しました！ご確認お願い致します。

---
+++ HelloWorld_IshiyamaJyunki/src/curriculum_B/Animal.java
@@ -0,0 +1,35 @@
+package curriculum_B;
+
+
+public class Animal {
+	private String name;
+	private double length;
+	private int speed;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getLength() {
+		return this.length;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return this.speed;
+	}
+}
+
+
--- HelloWorld_IshiyamaJyunki/src/curriculum_B/Qes5.java
+++ HelloWorld_IshiyamaJyunki/src/curriculum_B/Qes5.java
@@ -48,36 +48,7 @@ public class Qes5 {
 	 * Main.java 変数の中身を設定し、出力の処理を行う
 	 */
 
-	public static class Animal {
-		private String name;
-		private double length;
-		private int speed;
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getName() {
-			return this.name;
-
-		}
-
-		public void setLength(double length) {
-			this.length = length;
-		}
-
-		public double getLength() {
-			return this.length;
-		}
-
-		public void setSpeed(int speed) {
-			this.speed = speed;
-		}
-
-		public int getSpeed() {
-			return this.speed;
-		}
-	}
+	
 
 	//まとめて実行
 
